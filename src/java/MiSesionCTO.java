f3577b609432954fe86c95483d80f9d80743270f
Santiago29-6 <chavarrokevin29@gmail.com>
2022-02-10 08:25:58 -0500

    Subiendo ejercicio en clase

---
+++ src/java/MiSesionCTO.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Santiago
+ */
+@WebServlet(urlPatterns = {"/MiSesionCTO"})
+public class MiSesionCTO extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            
+            String accion = request.getParameter("accion");
+            
+            switch(accion){
+                case "validar":{
+                    String usuario = request.getParameter("txtUsuario");
+                    String clave = asegurarClave(request.getParameter("txtPass"));
+                    
+                    break;
+                }
+                case "salir":{
+                    break;
+                }
+            }
+        }
+    
+    private String asegurarClave(String clave){
+        String clave2 = "";
+        try {
+            MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+            sha256.reset();
+            sha256.update(clave.getBytes("uft8"));
+            clave2 = Base64.getEncoder().encodeToString(sha256.digest());
+            System.out.println("clave2: "+ clave2);
+            System.out.println("tamaño: "+clave2.length());
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
+            System.out.println("Error en cifrar "+ex.getMessage());
+        }
+        return clave2;
+    }
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- web/index.jsp
+++ web/index.jsp
@@ -11,9 +11,39 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>JSP Page</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
+              rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+              crossorigin="anonymous">
     </head>
     <body>
-        <h1>Hola mundo web</h1>
+        <div class="modal-dialog text-center">
+            <div class="col-lg-8">
+                <div class="modal-content">
+                    <div class="col-12">
+                        <img src="./Imagenes/user.jpg" alt="avatar" height="128" width="128">
+                    </div>
+                    <form action="MiSesionCTO?accion=validar" method="POST" class="col-12">
+                        <div class="form-group text-center">
+                            <p>Bienvenido al sistema de acceso</p>
+                        </div>
+                        <div class="form-group">
+                            <label>Usuario:</label>
+                            <input type="text" name="txtUsuario" placeholder="ejemplo@gmail.com" class="form-control" required/>
+                        <div class="form-group">
+                            <label>Clave de acesso:</label>
+                            <input type="password" name="txtPass" placeholder="clave" class="form-control" required>
+                        </div>
+                        <input type="submit" name="btnIngresar" value="Ingresar" class="btn btn-primary"/>
+                    </form>
+                </div>
+            </div>
+        </div>
         <a href="ProductoCTO?accion=listar_datos">listar productos</a>
+         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
+                integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" 
+                integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" 
+            integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     </body>
 </html>
